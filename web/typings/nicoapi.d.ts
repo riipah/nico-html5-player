2dbdd9850b4d0f9f9fa9f2715f80c3084d5d7c48
Riipah <riipah@vocadb.net>
2017-11-19 23:21:01 +0200

    first version

--- web/typings/nicoapi.d.ts
+++ web/typings/nicoapi.d.ts
@@ -4,12 +4,27 @@ declare namespace nico {
 		create(element: HTMLElement, watchId: string): Promise<NicoPlayer>;
 	}
 
+	export interface PlayerEvent {
+		data: EventData;
+	}
+
+	export interface EventData {
+		data: EventDataData;
+		eventName: EventName;
+	}
+
+	type EventName = "playerMetadataChange" | "playerStatusChange";
+
+	export interface EventDataData {
+		playerStatus: PlayerStatus;
+	}
+
 	export interface NicoPlayer {
 		play(): void;
 		pause(): void;
 	}
 
-	export enum PlayerStatus {
+	export const enum PlayerStatus {
 		Play = 2,
 		Pause = 3,
 		End = 4
--- web/viewModel.ts
+++ web/viewModel.ts
@@ -2,32 +2,100 @@ namespace nicoplayer {
 
 	export class ViewModel {
 
+		public isLoaded = ko.observable(false);
+		public isPlaying = ko.observable(false);
 		private playerFactory: nico.NicoPlayerFactory;
 		private player: nico.NicoPlayer;
-		public videoId = ko.observable("");
+		public videoId = ko.observable("http://www.nicovideo.jp/watch/sm4124456");
 
 		constructor(private readonly element: HTMLElement) {
 
-			window.onNicoPlayerFactoryReady = factory => this.playerFactory = factory;
+			this.init();
 
 		}	
 
-		public async createPlayer() {
-			const player = await this.playerFactory.create(this.element, this.videoId());			
+		public async createPlayer(videoId: string) {
+			const player = await this.playerFactory.create(this.element, videoId);			
 			return player;
+
 		}
 
-		public isPlaying = ko.observable(false);
+		private getVideoId() {
+
+			// www.nicovideo.jp/watch/sm32280113
+			const regex = /http:\/\/www.nicovideo.jp\/watch\/(\w+)/g;
+
+			const match = regex.exec(this.videoId());
+
+			if (!match || match.length < 2)
+				return null;
+
+			return match[1];
+
+		}
+
+		private async init() {
+
+			window.onNicoPlayerFactoryReady = factory => this.playerFactory = factory;
+
+			window.addEventListener("message", (e: nico.PlayerEvent) => {
+				switch (e.data.eventName) {
+					case "playerStatusChange": {
+						switch (e.data.data.playerStatus) {
+							case nico.PlayerStatus.Pause:
+								this.onPause();
+								break;
+							case nico.PlayerStatus.Play:
+								this.onPlay();
+								break;
+							case nico.PlayerStatus.End:
+								this.onEnd();
+						}
+					}
+				}
+			});
+
+			await $.getScript("https://embed.res.nimg.jp/js/api.js");
+			
+		}
+
+		public async load() {
+
+			const videoId = this.getVideoId();
+
+			if (!videoId) {
+				alert("Invalid video");
+				return;
+			}
+
+			this.element.innerHTML = "";
+			this.player = await this.createPlayer(videoId);
+			this.isLoaded(true);
+			this.play();
+
+		}
+
+		private onEnd = () => {
+			this.isPlaying(false);
+		}
+
+		private onPlay = () => {
+			this.isPlaying(true);
+		}
+
+		private onPause = () => {
+			this.isPlaying(false);
+		}
 
 		public pause() {
-			if (!this.player)
-				return;
-			this.player.pause();
+			if (this.player)
+				this.player.pause();
 		}
 
 		public async play() {
-			this.player = await this.createPlayer();
-			this.player.play();
+			if (this.player)
+				this.player.play();
+
 		}
 
 
